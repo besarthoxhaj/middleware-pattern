e82e8fb317f2e0d841ab0d87acb932bc671594e2
besartshyti <besartshyti@gmail.com>
2016-01-16 16:43:25 +0000

    add express example in a file

---
+++ express.js
@@ -0,0 +1,35 @@
+'use strict';
+
+function makeApp () {
+  var middlewareStore = [];
+  function app () { 
+    var index = 0;
+    function next (err) {
+      var layer = middlewareStore[index++];
+      if (layer) {
+        return layer(next);  
+      }
+    }
+    next();
+  }
+  app.add = function (fn) {
+    middlewareStore.push(fn);
+  }
+  return app;
+}
+
+var app = makeApp();
+
+app.add(function (next) {
+  console.log('One!');
+  next();
+});
+
+app.add(function (next) {
+  console.log('Two!');
+  next();
+});
+
+app();
+// One!
+// Two!
